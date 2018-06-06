debe5747b6e1ef25d577900f7523c818ea9e1c27
jimena <jimena.piano@globallogic.com>
2018-06-06 13:05:35 -0300

    discos.js en routes

---
+++ routes/discos.js
@@ -0,0 +1,50 @@
+var express = require('express');
+var router = express.Router();
+
+var generos = ['Classic Rock', 'Psychedelic Rock', 'Punk', 'Grunge', 'Soul', 'Folk', 'Clasica', 'Alternativo', 'Pop', 'Industrial', 'EBM'];
+
+var disco = [{ 	titulo: 'Unplugged in NY',
+				autor: 'Nirvana',
+            	genero: '3',
+			   	img:'images/harry_potter.png',
+			   	año: '1995',
+			   	descripcion: 'Con clásicos como The Man Who Sold The World'
+			 },
+			 { 	titulo: 'Harry Potter 1',
+				autor: '001',
+            	genero: '$ 250',
+			   	img:'198432',
+			   	año: 'images/harry_potter.png',
+			   	descripcion: 'Primer libro de la saga Harry Potter'
+			 },
+			 { 	titulo: 'Harry Potter 1',
+				autor: '001',
+            	genero: '$ 250',
+			   	img:'198432',
+			   	año: 'images/harry_potter.png',
+			   	descripcion: 'Primer libro de la saga Harry Potter'
+			 },
+			 { 	titulo: 'Harry Potter 1',
+				autor: '001',
+            	genero: '$ 250',
+			   	img:'198432',
+			   	año: 'images/harry_potter.png',
+			   	descripcion: 'Primer libro de la saga Harry Potter'
+			 },
+			 { 	titulo: 'Harry Potter 1',
+				autor: '001',
+            	genero: '$ 250',
+			   	img:'198432',
+			   	año: 'images/harry_potter.png',
+			   	descripcion: 'Primer libro de la saga Harry Potter'
+			 },
+			 { 	titulo: 'Harry Potter 1',
+				autor: '001',
+            	genero: '$ 250',
+			   	img:'198432',
+			   	año: 'images/harry_potter.png',
+			   	descripcion: 'Primer libro de la saga Harry Potter'
+			 },
+			 ];
+
+			 disco{nombre: ‘Unplugged’,artista:’Alejandro Sanz’,genero:id}
