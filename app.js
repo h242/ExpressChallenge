2156c52c726abbf6c3e12184ccaaa07825e32a1d
Sofía Candela Suvercase <sofiandela@MacBook-Air-de-Sofia.local>
2018-06-08 01:07:37 -0300

    Rutas

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var generosRouter = require('./routes/generos');
+var discosRouter = require('./routes/discos');
 
 var app = express();
 
@@ -21,6 +23,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/generos', generosRouter);
+app.use('/discos', discosRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
